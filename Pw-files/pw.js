66df979942700c5f90af28be61b692fd5c77baaa
KauaSouzaSantos <kaua.santos71@etec.sp.gov.br>
2022-12-04 13:27:16 -0300

    Home Atualizada

--- Pw-files/pw.js
+++
@@ -1,6 +0,0 @@
-const mainContainer = document.querySelector(".main-container");
-
-function mover() {
-  mainContainer.style.transform = "translatex(-100%)";
-  mainContainer.style.display = "none";
-}
