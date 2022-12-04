a8f83f62394ef8637c7b2ed81c6ec7b7ef856f4f
KauaSouzaSantos <kaua.santos71@etec.sp.gov.br>
2022-12-04 11:39:28 -0300

    Adicionado primeira versão da página Home

---
+++ Pw-files/pw.js
@@ -0,0 +1,6 @@
+const mainContainer = document.querySelector(".main-container");
+
+function mover() {
+  mainContainer.style.transform = "translatex(-100%)";
+  mainContainer.style.display = "none";
+}
